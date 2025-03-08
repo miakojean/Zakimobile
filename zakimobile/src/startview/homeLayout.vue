059521a477ba20a59de0c088c392e12509a8fac4
miakojean <miakojeanyves@gmail.com>
2025-03-08 03:00:05 +0000

    svg commit

---
+++ zakimobile/src/startview/homeLayout.vue
@@ -0,0 +1,29 @@
+<template>
+  <ionPage>
+        <div class="main__container">
+            <div class="main__picture">
+
+            </div>
+
+            <div class="main__text">
+                
+            </div>
+        </div>
+  </ionPage>
+</template>
+
+<script>
+import { IonPage } from '@ionic/vue';
+import { defineComponent } from 'vue';
+export default defineComponent({
+
+    components: {
+        IonPage,
+    }
+
+})
+</script>
+
+<style>
+
+</style>
