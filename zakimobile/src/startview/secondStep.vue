63b40cc083883740db8bc4ad1ea35cfcb97a60a5
Yvan <anguiyvan@gmail.com>
2025-03-08 02:58:52 +0100

    initialisation des pages

---
+++ zakimobile/src/startview/secondStep.vue
@@ -0,0 +1,30 @@
+<template>
+    <ion-page>
+        <div class="main__container">
+            <div class="main__picture">
+
+
+            </div>
+            <div class="main__text">
+                <h2>Commandez!!</h2>
+                <p>Découvrez vos ingrédients favoris sur notre application</p>
+
+            </div>
+            <button></button>
+        </div>
+    </ion-page>
+  
+</template>
+
+<script>
+import { IonPage } from '@ionic/vue';
+import { defineComponent } from 'vue';
+export default defineComponent({
+    components: {IonPage},
+
+})
+</script>
+
+<style>
+
+</style>
--- zakimobile/src/views/HomePage.vue
+++ zakimobile/src/views/HomePage.vue
@@ -1,22 +1,14 @@
 <template>
-
+  
   <ion-page>
     <h2>Bienvenu au pays mon fils</h2>
   </ion-page>
-  
 </template>
 
 <script>
-import { IonPage } from '@ionic/vue'
-import { defineComponent } from 'vue'
+export default {
 
-export default defineComponent({
-
-  components: {
-    IonPage,
-  }
-
-})
+}
 </script>
 
 <style>
