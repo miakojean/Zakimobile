d2eb63049d773587d83c11f3f5003ee18d26be0d
Yvan <anguiyvan@gmail.com>
2025-03-09 01:38:23 +0100

    thirdStep

--- zakimobile/src/startview/thirdStep.vue
+++ zakimobile/src/startview/thirdStep.vue
@@ -7,16 +7,20 @@
         <div class="main__text">
             <h2>Commencer</h2>
             <p>Facilitez vous la vie en nous confiant vos courses</p>
+            <mainButton/>
         </div>
-        <button></button>
     </div>
    </ion-page>
 </template>
 
 <script>
-export default {
+import mainButton from '../button/mainButton.vue';
+import { IonPage } from '@ionic/vue';
+import { defineComponent } from 'vue';
+export default defineComponent({
+    components: {IonPage, mainButton},
 
-}
+})
 </script>
 
 <style>
