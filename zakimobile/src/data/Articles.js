1d22cac5bc53ad7bbcd921ee522e274ce1262a3d
Yvan <anguiyvan@gmail.com>
2025-03-18 19:33:31 +0100

    Articledetail

--- zakimobile/src/data/Articles.js
+++ zakimobile/src/data/Articles.js
@@ -1,3 +1,9 @@
 const Fruits = [
-    
+    {
+        name: "banane douce",
+        prix: "300",
+        image: require("../assets/images/banane.jpg"),
+        description: "La banane douce, ou banane dessert, est un fruit tropical très populaire pour sa chair tendre, sucrée et parfumée.",
+           
+    }
 ]
--- zakimobile/src/router/index.js
+++ zakimobile/src/router/index.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from '@ionic/vue-router';
+import Articledetail from '../homeland/articledetail.vue';
 
 const routes = [
   {
@@ -29,6 +30,11 @@ const routes = [
     path: '/profile',
     name: 'profile',
     component: () => import('../homeland/ProfileLand.vue')
+  },
+  {
+    path: '/article-detail',
+    name: 'Articledetail',
+    component: () => import('../homeland/articledetail.vue')
   }
 ];
 
