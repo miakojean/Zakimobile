0b70d7c4b0a7d483fef39108674a2de3255abf74
miakojean <miakojeanyves@gmail.com>
2025-03-20 13:08:29 +0000

    loop for articles

--- zakimobile/src/data/Articles.js
+++ zakimobile/src/data/Articles.js
@@ -3,27 +3,37 @@ const Fruits = [
         name: "banane douce",
         prix: 300,
         description: "La banane douce, ou banane dessert, est un fruit tropical très populaire pour sa chair tendre, sucrée et parfumée.",
+        metrics: 'Kg',
+        image: "../../data/articles/bananedouce.jpg"
            
     },
     {
         name: "Orange",
         prix: 300,
-        description: ""
+        description: "",
+        metrics: 'Kg',
+        image: "../data/articles/orange.jpg"
     },
     {
         name: "Ananas",
         prix: 300,
-        description: ""
+        description: "",
+        metrics: 'Kg',
+        image: "../data/Articles/ananas.jpg"
     },
     {
-        name: "Orange",
+        name: "Fraise",
         prix: 300,
-        description: ""
+        description: "",
+        metrics: 'Kg',
+        image: "../data/Articles/fraise.jpg"
     },
     {
         name: "Pomme",
         prix: 300,
-        description: ""
+        description: "",
+        metrics: 'Kg',
+        image: "../data/Articles/pomme.jpg"
     }
 ]
 
--- zakimobile/src/homeland/homepage.vue
+++ zakimobile/src/homeland/homepage.vue
@@ -5,29 +5,18 @@
       <resarchBox/>
       <suggestionLists/>
       <div class="articles__container">
-        <div class="about__articles">
-          <img class="articles__pictures" src="../data/Articles/bananedouce.jpg" alt="banane douce">
+        <div class="about__articles"  v-for="(fruit, index) in fruits" :key="index">
+          <img class="articles__pictures" :src=" fruit.image " :alt="fruit.name">
           <div class="info">
-            <p>Banane douce</p>
-            <span>1 Kg</span>
+            <p>{{ fruit.name }}</p>
+            <span>1 {{ fruit.metrics }}</span>
           </div>
-          <span>$ 12.00</span>
-        </div>
-        <div class="about__articles">
-          <img class="articles__pictures" src="../data/Articles/bananedouce.jpg" alt="banane douce">
-          <div class="info">
-            <p>Banane douce</p>
-            <span>1 Kg</span>
+          <span>{{ fruit.prix }} FCFA</span>
+          <div class="add__products">
+            <div class="add__logo">
+              <ion-icon name="add-circle-outline"></ion-icon>
+            </div>
           </div>
-          <span>$ 12.00</span>
-        </div>
-        <div class="about__articles">
-          <img class="articles__pictures" src="../data/Articles/bananedouce.jpg" alt="banane douce">
-          <div class="info">
-            <p>Banane douce</p>
-            <span>1 Kg</span>
-          </div>
-          <span>$ 12.00</span>
         </div>
       </div>
     </IonContent>
@@ -36,7 +25,8 @@
 
 <script>
 import { defineComponent, ref, onMounted } from 'vue'; // Import defineComponent
-import { IonPage, IonContent } from '@ionic/vue';
+import { IonPage, IonContent, IonIcon } from '@ionic/vue';
+import { addCircleOutline } from 'ionicons/icons';
 import HeaderLayout from '../components/tools/headerLayout.vue';
 import Fruits from '../data/Articles';
 import MainButton from '../button/mainButton.vue';
@@ -47,16 +37,18 @@ export default defineComponent({
   components: {
     IonPage,
     IonContent,
+    IonIcon,
     HeaderLayout,
     MainButton,
     resarchBox,
-    suggestionLists
+    suggestionLists,
+    addCircleOutline
   },
   setup() {
     const fruits = ref(Fruits); // Use ref to make it reactive
 
     const voirTableau = () => {
-      console.log(fruits.value); // Access the value of ref using .value
+      console.log(fruits.value[0]); // Access the value of ref using .value
     };
 
     // Use onMounted inside setup
@@ -75,12 +67,60 @@ export default defineComponent({
 <style>
 
 .about__articles{
-  border: 1px solid gray;
   border-radius: 1rem;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  gap: 0;
+  box-shadow: 0 4px 4px 0 rgba(0, 0, 0, 0.2), 0 6px 8px 0 rgba(0, 0, 0, 0.19);
 }
 
+.about__articles span {
+  color: #058C42;
+  font-weight: 600;
+  text-align: start;
+}
+.info{
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0.2rem;
+}
+
+.info p{
+  font-weight: 600;
+  color: #058C42;
+}
+
+.info span {
+  font-weight: 600;
+  color: #616161;
+  font-size: 0.9rem;
+}
+
+.add__products{
+  width: 100%;
+  color: #616161;
+  display: flex;
+  justify-content: end;
+
+}
+
+.add__logo{
+  padding: 0.5rem;
+  background: #058C42;
+  border-radius: 1rem 0 1rem 0 ;
+  width: 50px;
+  display: flex;
+  flex-direction: column ;
+  align-items: center;
+  justify-content: center;
+}
+
+.add__products ion-icon {
+  font-size: 24px; /* Adjust size as needed */
+  color: #f3f3f3;  /* Ensure the color is visible */
+}
 </style>
