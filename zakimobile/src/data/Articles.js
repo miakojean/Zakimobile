42be091429ef44f1e91a6cd8ef0bddea42824ea3
Yvan <anguiyvan@gmail.com>
2025-03-20 13:25:22 +0100

    Articles

--- zakimobile/src/data/Articles.js
+++ zakimobile/src/data/Articles.js
@@ -1,39 +1,44 @@
+import bananeDouceImage from '../assets/Articles/bananedouce.jpg';
+import orangeImage from '../assets/Articles/orange.jpg';
+import ananasImage from '../assets/Articles/ananas.jpg';
+import fraiseImage from '../assets/Articles/fraise.jpg';
+import pommeImage from '../assets/Articles/pomme.jpg';
+
 const Fruits = [
     {
         name: "banane douce",
         prix: 300,
         description: "La banane douce, ou banane dessert, est un fruit tropical très populaire pour sa chair tendre, sucrée et parfumée.",
         metrics: 'Kg',
-        image: "../../data/articles/bananedouce.jpg"
-           
+        image: bananeDouceImage
     },
     {
         name: "Orange",
         prix: 300,
         description: "",
         metrics: 'Kg',
-        image: "../data/articles/orange.jpg"
+        image: orangeImage
     },
     {
         name: "Ananas",
         prix: 300,
         description: "",
         metrics: 'Kg',
-        image: "../data/Articles/ananas.jpg"
+        image: ananasImage
     },
     {
         name: "Fraise",
         prix: 300,
         description: "",
         metrics: 'Kg',
-        image: "../data/Articles/fraise.jpg"
+        image: fraiseImage
     },
     {
         name: "Pomme",
         prix: 300,
         description: "",
         metrics: 'Kg',
-        image: "../data/Articles/pomme.jpg"
+        image: pommeImage
     }
 ]
 
